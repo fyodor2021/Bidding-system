860f2e051d99940e4fa608bb29e44a331e7efaed
Vedoor Barakat <101388514@georgebrown.ca>
2023-02-18 09:48:09 -0500

    adding the necessary views and controllers

---
+++ Assignment1Group26/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1Group26.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment1Group26/Controllers/RegisterationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1Group26.Controllers
+{
+    public class RegisterationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
