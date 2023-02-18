b17ea5aedb7f52c6e72ac6caca65a1140e45703e
Vedoor Barakat <101388514@georgebrown.ca>
2023-02-18 09:06:17 -0500

    adding the reset of the project files

---
+++ Assignment1Group26/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment1Group26.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
