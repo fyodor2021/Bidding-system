0f431abb3d85a04e347e00f7ef002d41c8b0bef5
Vedoor Barakat <101388514@georgebrown.ca>
2023-02-18 09:42:04 -0500

    adding connection to the database

---
+++ Assignment1Group26/Models/HomeDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Assignment1Group26.Models
+{
+    public class HomeDbContext :DbContext
+    {
+        public HomeDbContext(DbContextOptions<HomeDbContext> options) : base(options) { }
+
+    }
+}
--- Assignment1Group26/Program.cs
+++ Assignment1Group26/Program.cs
@@ -1,8 +1,12 @@
+using Assignment1Group26.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<HomeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
