363940389f186ed16a4a89ac57bed33b71c0eaa3
Josefin Abdulaziz <101390844@georgebrown.ca>
2023-02-18 11:53:14 -0500

    creating category columns and assetCondition columns

--- Assignment1Group26/Models/AssetCondition.cs
+++ Assignment1Group26/Models/AssetCondition.cs
@@ -1,6 +1,12 @@
-﻿namespace Assignment1Group26.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1Group26.Models
 {
     public class AssetCondition
     {
+        [Key]
+        public int AssetConditionId { get; set; }
+        
+        public string AssetConditionName { get; set; }
     }
 }
--- Assignment1Group26/Models/Category.cs
+++ Assignment1Group26/Models/Category.cs
@@ -1,6 +1,11 @@
-﻿namespace Assignment1Group26.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1Group26.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
     }
 }
