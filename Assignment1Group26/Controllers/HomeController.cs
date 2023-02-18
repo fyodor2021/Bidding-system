420d9c84e4a05339bf720f61618abd570811cce3
Vedoor Barakat <101388514@georgebrown.ca>
2023-02-18 09:31:12 -0500

    deleting unnecessary files in the project

--- Assignment1Group26/Controllers/HomeController.cs
+++ Assignment1Group26/Controllers/HomeController.cs
@@ -1,32 +1,16 @@
-﻿using Assignment1Group26.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Assignment1Group26.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+   
     }
 }
--- Assignment1Group26/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment1Group26.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
