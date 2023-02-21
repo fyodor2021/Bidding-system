d7afdfae694d13caf05ac871d061155290e4e52a
Josefin Abdulaziz <101390844@georgebrown.ca>
2023-02-21 03:10:40 -0500

    creation of Sell,Edit and add bids actions/views

--- Assignment1Group26/Models/ApplicationDbContext.cs
+++ Assignment1Group26/Models/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Assignment1Group26.Models
@@ -10,7 +11,7 @@ namespace Assignment1Group26.Models
         public DbSet<Bid> bids { get; set; }
         public DbSet<Category> categories { get; set; }
         public DbSet<Client> clients { get; set; }
-        public DbSet<AssetCondition> AssetConditions { get; set; }
+        public DbSet<AssetCondition> assetConditions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -24,108 +25,12 @@ namespace Assignment1Group26.Models
 
             modelBuilder.Entity<AssetCondition>().HasData(
                     new AssetCondition { AssetConditionId = 1, AssetConditionName = "New" },
-                    new AssetCondition { AssetConditionId = 3, AssetConditionName = "Lightly Used" },
-                    new AssetCondition { AssetConditionId = 4, AssetConditionName = "Used" }
+                    new AssetCondition { AssetConditionId = 2, AssetConditionName = "Lightly Used" },
+                    new AssetCondition { AssetConditionId = 3, AssetConditionName = "Used" }
 
                 );
 
-            modelBuilder.Entity<Bid>().HasData(
-
-                    new Bid
-                    {
-                        BidId = 1,
-                        BidName = "Zara CROP KNIT TURTLENECK SWEATER",
-                        BidDescription = "Long sleeve turtleneck sweater",
-                        BidCost = 20,
-                        BidStartDate = DateTime.Now,
-                        BidEndDate = new DateTime(2023, 5, 16),
-                        AssetConditionId = 1,
-                        CategoryId = 1,
-                        ClientId = 1
-
-
-                    },
-                    new Bid
-                    {
-                        BidId = 2,
-                        BidName = "Zara CROP KNIT TURTLENECK SWEATER",
-                        BidDescription = "Long sleeve turtleneck sweater",
-                        BidCost = 20,
-                        BidStartDate = DateTime.Now,
-                        BidEndDate = new DateTime(2023, 5, 16),
-                        AssetConditionId = 1,
-                        CategoryId = 1,
-                        ClientId = 1
-
-                    },
-                    new Bid
-                    {
-                        BidId = 3,
-                        BidName = "Zara CROP KNIT TURTLENECK SWEATER",
-                        BidDescription = "Long sleeve turtleneck sweater",
-                        BidCost = 20,
-                        BidStartDate = DateTime.Now,
-                        BidEndDate = new DateTime(2023, 5, 16),
-                        AssetConditionId = 1,
-                        CategoryId = 1,
-                        ClientId = 2
-
-                    },
-                    new Bid
-                    {
-                        BidId = 4,
-                        BidName = "Zara CROP KNIT TURTLENECK SWEATER",
-                        BidDescription = "Long sleeve turtleneck sweater",
-                        BidCost = 20,
-                        BidStartDate = DateTime.Now,
-                        BidEndDate = new DateTime(2023, 5, 16),
-                        AssetConditionId = 1,
-                        CategoryId = 1,
-                        ClientId = 1
-
-                    },
-                    new Bid
-                    {
-                        BidId = 5,
-                        BidName = "Zara CROP KNIT TURTLENECK SWEATER",
-                        BidDescription = "Long sleeve turtleneck sweater",
-                        BidCost = 20,
-                        BidStartDate = DateTime.Now,
-                        BidEndDate = new DateTime(2023, 5, 16),
-                        AssetConditionId = 1,
-                        CategoryId = 1,
-                        ClientId = 1
-
-                    },
-                    new Bid
-                    {
-                        BidId = 6,
-                        BidName = "Zara CROP KNIT TURTLENECK SWEATER",
-                        BidDescription = "Long sleeve turtleneck sweater",
-                        BidCost = 20,
-                        BidStartDate = DateTime.Now,
-                        BidEndDate = new DateTime(2023, 5, 16),
-                        AssetConditionId = 1,
-                        CategoryId = 1,
-                        ClientId = 1
-
-                    },
-                    new Bid
-                    {
-                        BidId = 7,
-                        BidName = "Zara CROP KNIT TURTLENECK SWEATER",
-                        BidDescription = "Long sleeve turtleneck sweater",
-                        BidCost = 20,
-                        BidStartDate = DateTime.Now,
-                        BidEndDate = new DateTime(2023, 5, 16),
-                        AssetConditionId = 1,
-                        CategoryId = 1,
-                        ClientId = 2
-                    }
-
-
-
-                );
+        
 
             modelBuilder.Entity<Client>().HasData(
 
