0c384987c1d46ffb2f9a93f19bd4d48cb0378f87
Vedoor Barakat <101388514@georgebrown.ca>
2023-04-11 12:39:22 -0400

    live counter implemented

--- Assignment1Group26/wwwroot/js/site.js
+++ Assignment1Group26/wwwroot/js/site.js
@@ -1,11 +1,4 @@
-﻿import Timer from "./Timer.js";
-
-new Timer(
-    document.querySelector(".timer")
-);
-
-
-
+﻿"use strict";
 
 function unCheck() {
 
@@ -17,3 +10,42 @@ function unCheck() {
         }
     }
 }
+
+setInterval(function () {
+    var timers = document.getElementsByClassName("timer");
+    
+
+    for (let timerCounter = 0; timerCounter < timers.length; timerCounter++) {
+        var timer = timers[timerCounter];
+        var secondsDivs = timer.getElementsByClassName("seconds");
+        if (secondsDivs[0].innerHTML >= 1) {
+            console.log(secondsDivs[0]);
+            secondsDivs[0].innerHTML = secondsDivs[0].innerHTML - 1;
+        } else {
+            secondsDivs[0].innerHTML = 60;
+            var minutesDivs = timer.getElementsByClassName("minutes");
+            if (minutesDivs[0].innerHTML >= 1) {
+                minutesDivs[0].innerHTML = minutesDivs[0].innerHTML - 1;
+            } else {
+                minutesDivs[0].innerHTML = 60;
+                var hoursDivs = timer.getElementsByClassName("hours");
+                if (hoursDivs[0].innerHTML >= 1) {
+                    hoursDivs[0].innerHTML = hoursDivs[0].innerHTML - 1;
+                } else {
+                    hoursDivs[0].innerHTML = 60;
+
+                }
+            }
+        }
+        
+        
+        //for (let secondsCounter = 0; secondsCounter < timers.length; secondsCounter++) {
+        //    if (secondsDivs[secondsCounter].innerHTML >= 1) {
+        //        secondsDivs[secondsCounter].innerHTML = secondsDivs[secondsCounter].innerHTML - 1
+        //    }
+        //}
+    }
+}, 1000);
+//setInterval(document.getElementsByClassName(".hours"), 1)
+//setInterval(document.getElementsByClassName(".minutes"), 1)
+//setInterval(document.getElementsByClassName(".seconds"), 1)
